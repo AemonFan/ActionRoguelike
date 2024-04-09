ee7e2114c786288521219ddeae319c120218fe9b
AemonFan <1184627555@qq.com>
2024-04-09 13:34:54 +0800

    添加AI角色Minion类

---
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SAIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+ASAIController::ASAIController()
+{
+}
+
+void ASAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(ensure(BeHaviorTree))
+	{
+		RunBehaviorTree(BeHaviorTree);
+	}
+	
+	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	if(MyPawn)
+	{
+		if(GetBlackboardComponent())
+		{
+			// GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+			GetBlackboardComponent()->SetValueAsObject("MoveToActor", MyPawn);
+		}
+	}
+
+}
+
